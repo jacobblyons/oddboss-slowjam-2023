b80006babe7e1b20e75c6d07535f62bda5d86999
Jacob Lyons <lyons5609@gmail.com>
2023-11-14 00:03:34 -0800

    a whole bunch of npc stuff

---
+++ scripts/CarNpcController.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class CarNpcController : AnimatableBody3D
+{
+    [Export]
+    public float Speed = 10.0f;
+
+    [Export]
+    public PathFollow3D Path;
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        // move npc
+        Path.Progress += (float)(Speed * delta);
+        
+    }
+
+
+}
--- scripts/NpcController.cs
+++ scripts/NpcController.cs
@@ -1,65 +1,173 @@
 using System;
+using System.Linq;
 using Godot;
+
 
 public partial class NpcController : CharacterBody3D
 {
-    private NavigationAgent3D _navigationAgent;
-
     [Export]
-    public float _movementSpeed =2.0f;
-
+    public float walkSpeed = 2.0f;
     [Export]
-    public TargetNode target;
-
+    public float runSpeed = 5.0f;
     [Export]
-    private Godot.Vector3 _movementTargetPosition = new Godot.Vector3(3.0f, 0.0f, 2.0f);
+    public float rotateSpeed = 2.0f;
+    [Export]
+    public float stamina = 2.0f;
+    [Export]
+    public TargetNode originalTarget;
+    [Export]
+    public float distanceToTargetOppositeOfAlien = 5.0f;
+    [Export]
+    public float distanceBeforeAbandoningEscape = 5.0f;
 
-    public Godot.Vector3 MovementTarget
-    {
-        get { return _navigationAgent.TargetPosition; }
-        set { _navigationAgent.TargetPosition = value; }
-    }
+    private NavigationAgent3D navigationAgent;
+    private Area3D playerDetectionBoundary;
+    private Area3D escapeDetectionBoundary;
+    private Area3D escapeReachedBoundary;
+    private TargetNode currentTarget;
+    private Node3D alienBeingFledFrom;
+    private NpcState state = NpcState.Traveling;
+    private float timeSinceBecameFearful = 0.0f;
+
 
     public override void _Ready()
     {
         base._Ready();
+        navigationAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
 
-        _navigationAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+        escapeDetectionBoundary = GetNode<Area3D>("EscapeDetectionBoundary");
+        escapeReachedBoundary = GetNode<Area3D>("EscapeInReachBoundary");
+        playerDetectionBoundary = GetNode<Area3D>("PlayerDetectionBoundary");
+        playerDetectionBoundary.BodyEntered += OnPlayerDetectedInBounds;
+        playerDetectionBoundary.BodyExited += OnPlayerLeftBounds;
 
-        // These values need to be adjusted for the actor's speed
-        // and the navigation layout.
-        _navigationAgent.PathDesiredDistance = 0.5f;
-        _navigationAgent.TargetDesiredDistance = 0.5f;
-    
         // Make sure to not await during _Ready.
         Callable.From(ActorSetup).CallDeferred();
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
-
-        if (_navigationAgent.IsNavigationFinished())
-        {
-            QueueFree();
-        }
-
-        _navigationAgent.TargetPosition = target.GlobalPosition;
-
-        Godot.Vector3 currentAgentPosition = GlobalTransform.Origin;
-        Godot.Vector3 nextPathPosition = _navigationAgent.GetNextPathPosition();
-        Godot.Vector3 newVelocity = (nextPathPosition - currentAgentPosition).Normalized();
-        
-        Velocity = newVelocity * _movementSpeed;
-        MoveAndSlide();
     }
 
     private async void ActorSetup()
     {
         // Wait for the first physics frame so the NavigationServer can sync.
         await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
-
-        // Now that the navigation map is no longer empty, set the movement target.
-        MovementTarget = _movementTargetPosition;
+        navigationAgent.TargetPosition = originalTarget.GlobalPosition;
     }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (navigationAgent.IsNavigationFinished())
+        {
+            QueueFree();
+        }
+
+        navigationAgent.TargetPosition = GetTargetPosition();
+
+        Vector3 currentAgentPosition = GlobalTransform.Origin;
+        Vector3 nextPathPosition = navigationAgent.GetNextPathPosition();
+        Vector3 newVelocity = (nextPathPosition - currentAgentPosition).Normalized();
+
+        // set speed based on state
+        switch (state)
+        {
+            case NpcState.Fearful:
+            case NpcState.FleeingFromAlien:
+                Velocity = newVelocity * runSpeed;
+                break;
+
+            case NpcState.Traveling:
+            case NpcState.Loitering:
+            default:
+                Velocity = newVelocity * runSpeed;
+                break;
+        }
+        // move npc
+        // Calculate target rotation
+        var newTransform = GlobalTransform.LookingAt(GlobalPosition - newVelocity, Vector3.Up);
+        Quaternion targetRotation = new Quaternion(newTransform.Basis);
+
+        // Interpolate rotation
+        Quaternion currentRotation = new Quaternion(GlobalTransform.Basis);
+        Quaternion newRotation = currentRotation.Slerp(targetRotation, (float)(rotateSpeed * delta));
+
+        // Apply rotation
+        GlobalRotation = newRotation.GetEuler();
+
+        //MoveAndSlide();
+        MoveAndCollide(Velocity * (float)delta);
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        switch (state)
+        {
+            case NpcState.Fearful:
+                return GetEscapeTarget() ?? originalTarget.GlobalPosition;
+            case NpcState.FleeingFromAlien:
+                return GetDistanceFromAlien() > distanceBeforeAbandoningEscape // if alien is far enough away, go to escape
+                            ? GetEscapeTarget() ?? GetTargetAwayFromAlien() 
+                            : GetTargetAwayFromAlien();
+            case NpcState.Traveling:
+            default:
+                return originalTarget.GlobalPosition;
+        }
+    }
+
+    private Vector3 GetTargetAwayFromAlien()
+    {
+        Vector3 characterPosition = GlobalPosition;
+        Vector3 enemyPosition = alienBeingFledFrom.GlobalPosition;
+        Vector3 directionFromEnemy = (characterPosition - enemyPosition).Normalized();
+        return characterPosition + directionFromEnemy * distanceToTargetOppositeOfAlien;
+    }
+
+    private Vector3? GetEscapeTarget()
+    {
+        // look for an esape in the escape detection boundary
+        var escapes = escapeDetectionBoundary.GetOverlappingAreas();
+        return escapes.OrderBy(e => (e.GlobalPosition - GlobalPosition).Length()).FirstOrDefault()?.GlobalPosition;
+    }
+
+    private float GetDistanceFromAlien()
+    {
+        if (alienBeingFledFrom == null)
+            return float.MaxValue;
+        return (GlobalPosition - alienBeingFledFrom.GlobalPosition).Length();
+    }
+
+#region Events
+    private void OnPlayerDetectedInBounds(Node3D body)
+    {
+        if (body is not PlayerFPSController)
+        {
+            GD.PrintErr("NpcController.OnPlayerDetected: body is not PlayerFPSController. Only players should be detected on layer 2.");
+            return;
+        }
+
+        state = NpcState.FleeingFromAlien;
+        alienBeingFledFrom = body;
+    }
+
+    private void OnPlayerLeftBounds(Node3D body)
+    {
+        if (body is not PlayerFPSController)
+        {
+            GD.PrintErr("NpcController.OnPlayerDetected: body is not PlayerFPSController. Only players should be detected on layer 2.");
+            return;
+        }
+
+        state = NpcState.Fearful;
+        timeSinceBecameFearful = Time.GetTicksMsec();
+        alienBeingFledFrom = null;
+    }
+
+    private void OnEscapeReached(Node body)
+    {
+        if(state != NpcState.Fearful && state != NpcState.FleeingFromAlien)
+            return;
+
+        QueueFree();
+    }
+#endregion
+
 }
